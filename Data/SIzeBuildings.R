01aa965eaf006dee58a4ac447bb27b04126a02fe
Israel Martinez Hernandez <israel.m.hdz@gmail.com>
2025-12-15 10:26:52 +0000

    Update SIzeBuildings.R

--- Data/SIzeBuildings.R
+++ Data/SIzeBuildings.R
@@ -1,20 +1,21 @@
 
 # Charles Carter (m^2)
 Floor.Area <- 3835
-Year Built <- 2011
+Year.Built <- 2011
 
 # Engineering Building 
 
 Floor.Area <-	4620
-Year Built <- 2014
+Year.Built <- 2014
 
 # Library
 Floor.Area <- 14882 
-Year Built <- 1966
+Year.Built <- 1966
+
 #PSC
 Floor.Area <- 985
-Year Built <- 2008
+Year.Built <- 2008
 
 #ScienceTech
 Floor.Area <- 7349
-Year Built <- 1972
+Year.Built <- 1972
